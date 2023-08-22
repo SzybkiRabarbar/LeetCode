59785512c74c14bd8cc25909f6d21fc1901ce263
SzybkiRabarbar <maksjoni36@gmail.com>
2023-08-22 16:05:41 +0200

    33. Search in Rotated Sorted Array

---
+++ 2023-08-22#33 Search in Rotated Sorted Array.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+class Solution: # T: 60.11% M: 92.87%
+    def search(self, nums: list[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if target == nums[mid]:
+                return mid
+            if nums[left] <= nums[mid]:
+                if target > nums[mid] or target < nums[left]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if target < nums[mid] or target > nums[right]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1
