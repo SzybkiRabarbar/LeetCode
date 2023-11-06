2163dc8e87f5aaed2974485ce9dad28403efad6d
SzybkiRabarbar <maksjoni36@gmail.com>
2023-11-06 10:25:43 +0100

    REDO 39. Combination Sum

--- 2023-10-27_39 Combination Sum.py
+++ 2023-10-27_39 Combination Sum.py
@@ -18,4 +18,26 @@ class Solution: # T: 39.45% M: 12.19%
                         if q[-1] <= num:
                             queue.append(q + [num])
         return result
-        
+
+class Solution: # T: 77.26% M: 30.37%
+    def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
+        combs = [[]]
+        res = []
+        for c in candidates:
+            if c > target:
+                continue
+            if c == target:
+                res.append([c])
+                continue
+            for i in range(len(combs)):
+                sum_ = sum(combs[i]) + c
+                if sum_ == target:
+                    res.append(combs[i] + [c])
+                elif sum_ < target:
+                    combs.append(combs[i] + [c])
+                    while (sum_ + c) < target:
+                        sum_ += c
+                        combs.append(combs[-1] + [c])
+                    if (sum_ + c) == target:
+                        res.append(combs[-1] + [c])
+        return res
