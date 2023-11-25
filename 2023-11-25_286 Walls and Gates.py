e917db777c6f7fea6df27bf4abac02ca9f3db3f5
SzybkiRabarbar <maksjoni36@gmail.com>
2023-11-25 16:28:13 +0100

    286. Walls and Gate

--- 2023-11-25_286 Walls and Gates.py
+++ 2023-11-25_286 Walls and Gates.py
@@ -3,7 +3,7 @@
 
 from collections import deque
 
-class Solution:
+class Solution1:
     def walls_and_gates(self, rooms: list[list[int]]):
         ln_row = len(rooms)
         ln_col = len(rooms[0])
@@ -11,14 +11,18 @@ class Solution:
             for y in range(len(rooms[0])):
                 que = deque([(x, y)])
                 result = -1
+                seen = set()
                 exit_ = False
                 while que and not exit_:
                     result += 1
                     for _ in range(len(que)):
                         i, j = que.popleft()
-                        if not (i >= 0 and j >= 0 and
-                            i < ln_row and j < ln_col
-                        ): continue
+                        if not (i >= 0 and j >= 0 and i < ln_row and j < ln_col):
+                            continue
+                        elif (i, j) in seen:
+                            continue
+                        else:
+                            seen.add((i, j))
                         
                         val = rooms[i][j]
                         if not val:
@@ -28,17 +32,46 @@ class Solution:
                             for ai, aj in [(-1,0), (1,0), (0,-1), (0,1)]:
                                 que.append((i + ai, j + aj))
                 
-                if result: rooms[x][y] = result
-        
+                if result and exit_: rooms[x][y] = result
+                
+class Solution:
+    #@ https://www.youtube.com/watch?v=e69C6xhiSQE
+    def walls_and_gates(self, rooms: list[list[int]]):
+        ROWS, COLS = len(rooms), len(rooms[0])
+        visit = set()
+        q = deque()
+
+        def addRooms(r, c):
+            if (
+                min(r, c) < 0
+                or r == ROWS
+                or c == COLS
+                or (r, c) in visit
+                or rooms[r][c] == -1
+            ):
+                return
+            visit.add((r, c))
+            q.append([r, c])
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if rooms[r][c] == 0:
+                    q.append([r, c])
+                    visit.add((r, c))
+
+        dist = 0
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                rooms[r][c] = dist
+                addRooms(r + 1, c)
+                addRooms(r - 1, c)
+                addRooms(r, c + 1)
+                addRooms(r, c - 1)
+            dist += 1
+
 if __name__=="__main__":
     s = Solution()
     inp = [[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1],[2147483647,-1,2147483647,-1],[0,-1,2147483647,2147483647]]
     s.walls_and_gates(inp)
     print(inp)
-             
-"""
-Input:
-[[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1],[2147483647,-1,2147483647,-1],[0,-1,2147483647,2147483647]]
-Output:
-[[3,-1,0,1],[2,2,1,-1],[1,-1,2,-1],[0,-1,3,4]]
-"""
