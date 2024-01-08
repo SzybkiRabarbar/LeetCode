73fedcc64cfda946972ac31604f6b67ec623d8eb
SzybkiRabarbar <maksjoni36@gmail.com>
2024-01-08 23:47:25 +0100

    1584. Min Cost to Connect All Points

---
+++ 2024-01-08_1584 Min Cost to Connect All Points.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/min-cost-to-connect-all-points/
+from collections import heapq
+
+
+class Solution:  # T: 42.94% M: 15.50%
+    # https://www.youtube.com/watch?v=f7JOBJIC-NA
+    def minCostConnectPoints(self, points: list[list[int]]) -> int:
+        N = len(points)
+        adj = {i: [] for i in range(N)}  # i : list of [cost, node]
+        for i in range(N):
+            x1, y1 = points[i]
+            for j in range(i + 1, N):
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                adj[i].append([dist, j])
+                adj[j].append([dist, i])
+
+        # Prim's
+        res = 0
+        visit = set()
+        minH = [[0, 0]]  # [cost, point]
+        while len(visit) < N:
+            cost, i = heapq.heappop(minH)
+            if i in visit:
+                continue
+            res += cost
+            visit.add(i)
+            for neiCost, nei in adj[i]:
+                if nei not in visit:
+                    heapq.heappush(minH, [neiCost, nei])
+        return res
